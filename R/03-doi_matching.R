c3c7be24b120c655189b69ed125c8d9f805d1f49
gavieira <gabrieldeusdeth@gmail.com>
2023-11-02 17:28:57 -0300

    Fixed duplicate numbers in filenames and cleaned up '03-doi_matching.R'

--- R/03-doi_matching.R
+++ R/03-doi_matching.R
@@ -1,17 +1,16 @@
-#' Document matching using DOI (Digital Object Identifier) data for two dataframes
+#' Document matching using unique identifiers for two bibliographic datasets
+#'
+#' @description
+#' This function receives two lists of unique identifiers (e.g. DOI - Digital Object Identifier) and returns a list containing the matches of list1 against list2
+#' This step of the matching is also given a "fake score" that will be used as a tag to identify and exclude matched documents from subsequent doi matching steps, increasing overall efficiency of the whole process
 #'
 #' @param db1 - First bibliographic database in the comparison
 #' @param db2 - Second bibliographic database in the comparison
 #' @param n_threads - number of (logical) cores to be used in the matching
-#' @param doi_score - custom score value to help identify DOI matches in previous comparisons
+#' @param doi_score - custom score value to help identify DOI matches in subsequent comparisons
 #'
 #' @return a list containing match data
-# @export
 #'
-# @examples
-#Function that receives two lists of DOIs and returns a list containing the DOI matches of list1 against list2
-#We'll also give the DOI matching a "fake score" that will be used to identify which rows have been matched in previous pairwise comparisons
-
 doi_matching <- function(db1, db2, n_threads, doi_score = 2) {
   subset_db1 <- subset_db_for_doi_match(db1) #Extracting DOI info from db1
   subset_db2 <- subset_db_for_doi_match(db2) #Extracting DOI info from db2
